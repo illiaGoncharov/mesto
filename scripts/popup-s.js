e9f1df567e1f0ee2bfa89a56af3e10254095d5cf
illiaGoncharov <ilia.m.goncharov@gmail.com>
2021-02-27 20:13:22 +0300

    feat/lightbox-for-desk&mob && feat/smooth-op&close

--- scripts/popup-s.js
+++ scripts/popup-s.js
@@ -1,3 +1,70 @@
+// initial array
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+function delCard(e) {
+  const elt = e.target.closest(".card");
+  elt.remove();
+}
+
+function addCard(name, link) {
+  // get template & clone it 
+  const template = document.querySelector('#card').content;
+  let templateNew = template.cloneNode(true);
+
+  // apply vals
+  templateNew.querySelector('.card__title').innerText = name;
+  templateNew.querySelector('.card__image').src = link;
+  templateNew.querySelector('.card__image').alt = "На картинке изображен регион " + name;
+
+  // like button 
+  const likeB = templateNew.querySelector(".card__like-icon");
+  likeB.addEventListener("click", () => {
+    likeB.classList.toggle("card__like-icon_liked");
+  });
+
+  // delete
+  const delB = templateNew.querySelector(".card__delete-icon");
+  delB.addEventListener("click", delCard);
+
+  // lightbox
+  templateNew.querySelector('.card__image').addEventListener('click', () => {
+    openLB(link, name);
+  });
+
+  // add to document
+  document.querySelector('.cards').prepend(templateNew);
+}
+
+// init
+for (let i = 0; i < initialCards.length; i++) {
+  addCard(initialCards[i].name, initialCards[i].link);
+}
+
 // popup behavior
 function closePopup(target) {
   target.classList.remove("popup_opened");
@@ -5,6 +72,19 @@ function closePopup(target) {
 function openPopup(target) {
   target.classList.add("popup_opened");
 }
+
+const popupList = document.querySelectorAll(".popup");
+
+popupList.forEach((popup) => {
+  popup.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("popup_opened")) {
+      closePopup(popup);
+    }
+    if (evt.target.classList.contains("popup__close-button")) {
+      closePopup(popup);
+    }
+  });
+});
 
 // modals [edit & add]
 const popupE = document.querySelector('.popup_type_edit'),
@@ -77,4 +157,20 @@ addB.addEventListener('click', function() {
   openPopup(popupA);
 });
 formA.addEventListener('submit', getCard);
+
+// modal + elements [lightbox] 
+const popupLB = document.querySelector(".popup_type_lightbox"),
+      popupFig = popupLB.querySelector(".popup__img-figure"),
+      popupImg = popupFig.querySelector(".popup__img"),
+      popupCap = popupFig.querySelector(".popup__img-caption");
+
+function openLB(link, desc) {
+  openPopup(popupLB);
+  popupImg.src = link;
+  popupImg.alt = desc;
+  popupCap.textContent = desc;
+}
+
+
+
 
