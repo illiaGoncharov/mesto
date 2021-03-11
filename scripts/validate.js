2cee7ef46772086a0a0676bed7c4d22867bddd30
illia goncharov <ilia.m.goncharov@gmail.com>
2021-03-11 20:37:14 +0300

    feat: add validate.js file & link & basic js logic && ref: edit form markup

---
+++ scripts/validate.js
@@ -0,0 +1,19 @@
+const validationDOMStrings = {
+  formContainer: ".form",
+  formInput: ".form__input",
+  formSubmitButton: ".form__submit-button",
+  formInputNoValid: "form__input_type_no-valid",
+  formInputError: "form__input_type_error"
+};
+
+function showError(form, input, options) => {};
+
+function hideError(form, input, options) => {};
+
+function checkValidity(form, input, options) => {};
+
+function toggleButtonState(button, isValid, options) => {};
+
+function setEventListeners(form, options) => {};
+
+function enableValidation(options) => {};
