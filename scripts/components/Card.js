7ce70d4652f865124c1da952e0cd421ba8d9e254
illia goncharov <ilia.m.goncharov@gmail.com>
2021-08-31 20:06:40 +0300

    hot-fix: Revert to 56e05fced

---
+++ scripts/components/Card.js
@@ -0,0 +1,55 @@
+import { popupLightbox, popupImage, popupCaption } from '../index.js';
+
+export default class Card {
+	constructor(name, link, selector) {
+		this._name = name;
+		this._link = link;
+		this._selector = selector;
+	}
+
+	_getTemplate() {
+		const cardTemplate = document.querySelector(this._selector).content.cloneNode(true);
+		return cardTemplate;
+	}
+
+	_setEventListeners() {
+		this._card.querySelector('.card__image').addEventListener('click', () => {
+			this._openLightbox();
+		});
+
+		this._card.querySelector('.card__like-icon').addEventListener('click', (e) => {
+			e.target.classList.toggle("card__like-icon_liked");
+		});
+
+		this._card.querySelector('.card__delete-icon').addEventListener('click', (e) => {
+			e.target.closest(".card").remove();
+		});
+	}
+
+	_openLightbox() {
+		popupImage.src = this._link;
+		popupImage.alt = this._name;
+		popupCaption.textContent = this._name;
+		openPopup(popupLightbox);
+	}
+
+	generateCard() {
+		this._card = this._getTemplate();
+		this._setEventListeners();
+
+		const image = this._card.querySelector('.card__image');
+		const title = this._card.querySelector('.card__title');
+
+		image.src = this._link;
+		image.alt = this._name;
+		title.textContent = this._name;
+
+		return this._card;
+	}
+
+	tellData() {
+		console.log(this._name);
+		console.log(this._link);
+		console.log(this._selector);
+	}
+}
